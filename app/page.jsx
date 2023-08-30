d4f7bc18d5fa79f0f0e9a9c2f03dfbdfa09d9d51
cheveniko <nicolas.baquero@icloud.com>
2023-08-29 23:06:57 -0500

    upload files

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Users from "@/components/users";
+
+const fetchUsers = async () => {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
+};
+
+async function HomePage() {
+  const users = await fetchUsers();
+  return <Users users={users} />;
+}
+
+export default HomePage;
